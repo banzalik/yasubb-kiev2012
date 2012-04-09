a2481e0f550ddff89da0ef7bbcf1cb8e7961c6d1
Alexander Boythenko <banzalik@gmail.com>
2012-04-09 17:01:08 +0300

    hello word

---
+++ pages/index/index.bemjson.js
@@ -0,0 +1,11 @@
+({
+    block: 'b-page',
+    title: 'Yandex.Fotki',
+    head: [
+        { elem: 'css', url: 'index.css'},
+        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8' },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: 'index.js' }
+    ],
+    content: 'Hello Word'
+})
