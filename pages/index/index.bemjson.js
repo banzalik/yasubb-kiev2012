4e171c928718723528fb222d5e1f41063b49189d
Alexander Boythenko <banzalik@gmail.com>
2012-04-11 20:18:25 +0300

    добавил Яндекс.Метрику

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -16,5 +16,8 @@
         }
     },{
         block: 'b-social'
+    },{
+        block: 'i-metrica',
+        content: '<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter13851124 = new Ya.Metrika({id:13851124, enableAll: true, ut:"noindex", webvisor:true}); } catch(e) {} }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/13851124?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>'
     }]
 })
